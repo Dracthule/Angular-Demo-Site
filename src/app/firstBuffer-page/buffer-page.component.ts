d7722dad7eca9520fb8858473a238ac7162af97d
Dracthule <43589285+Dracthule@users.noreply.github.com>
2018-12-11 02:22:34 -0800

    quick changes for user access and age verification

---
+++ src/app/firstBuffer-page/buffer-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buffer-page',
+  templateUrl: './buffer-page.component.html',
+  styleUrls: ['./buffer-page.component.css']
+})
+export class BufferPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
