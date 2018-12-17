a45c6bad6735725477373e2ed78aacff1ee689a1
Darrell Curtright <d.curtright@dcinsites.co>
2018-12-16 18:39:46 -0800

    buffer-page adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { StoreComponent } from './main/store/store.component';
 import { SocialComponent } from './main/social/social.component';
 import { MiscComponent } from './main/misc/misc.component';
 import { PortfolioComponent } from './main/portfolio/portfolio.component';
-import { BufferPageComponent } from './buffer-page/buffer-page.component';
+import { BufferPageComponent } from './firstbuffer-page/buffer-page.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { AgeVerifComponent } from './age-verif/age-verif.component';
